db74c55e15fcc331f0513d86b81929fae69b7133
Raghi Ramachandran <raghir01@gmail.com>
2019-07-03 08:54:53 -0400

    Model created

--- U1M6Summative/src/main/java/com/company/U1M6Summative/model/Customer.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/model/Customer.java
@@ -1,4 +1,78 @@
 package com.company.U1M6Summative.model;
 
+import java.util.Objects;
+
 public class Customer {
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String company;
+    private String phone;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return getCustomerId() == customer.getCustomerId() &&
+                Objects.equals(getFirstName(), customer.getFirstName()) &&
+                Objects.equals(getLastName(), customer.getLastName()) &&
+                Objects.equals(getEmail(), customer.getEmail()) &&
+                Objects.equals(getCompany(), customer.getCompany()) &&
+                Objects.equals(getPhone(), customer.getPhone());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCustomerId(), getFirstName(), getLastName(), getEmail(), getCompany(), getPhone());
+
+    }
 }
--- U1M6Summative/src/main/java/com/company/U1M6Summative/model/InvoiceItem.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/model/InvoiceItem.java
@@ -1,4 +1,78 @@
 package com.company.U1M6Summative.model;
 
+import java.util.Objects;
+
 public class InvoiceItem {
+    private int invoiceItemId;
+    private int invoiceId;
+    private int itemId;
+    private int quantity;
+    private double unitRate;
+    private double discount;
+
+    public int getInvoiceItemId() {
+        return invoiceItemId;
+    }
+
+    public void setInvoiceItemId(int invoiceItemId) {
+        this.invoiceItemId = invoiceItemId;
+    }
+
+    public int getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(int invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getUnitRate() {
+        return unitRate;
+    }
+
+    public void setUnitRate(double unitRate) {
+        this.unitRate = unitRate;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceItem invoiceItem = (InvoiceItem) o;
+        return getInvoiceItemId() == invoiceItem.getInvoiceItemId() &&
+                Objects.equals(getInvoiceId(), invoiceItem.getInvoiceId()) &&
+                Objects.equals(getItemId(), invoiceItem.getItemId()) &&
+                Objects.equals(getQuantity(), invoiceItem.getQuantity()) &&
+                Objects.equals(getUnitRate(), invoiceItem.getUnitRate()) &&
+                Objects.equals(getDiscount(), invoiceItem.getDiscount());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getInvoiceItemId(), getInvoiceId(), getItemId(), getQuantity(), getUnitRate(), getDiscount());
+
+    }
 }
