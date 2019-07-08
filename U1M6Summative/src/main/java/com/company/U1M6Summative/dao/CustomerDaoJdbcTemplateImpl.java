46c6fbde0d50571dc020a5227a80c948fbf9b892
Perri Love <perri.love1188@gmail.com>
2019-07-08 10:48:27 -0400

    change to DAO

--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/CustomerDaoJdbcTemplateImpl.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/dao/CustomerDaoJdbcTemplateImpl.java
@@ -1,4 +1,48 @@
 package com.company.U1M6Summative.dao;
 
-public class CustomerDaoJdbcTemplateImpl {
+
+import com.company.U1M6Summative.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CustomerDaoJdbcTemplateImpl implements CustomerDao {
+
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public CustomerDaoJdbcTemplateImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+
+    @Override
+    public Customer addCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer getCustomer(int customerId) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public void updateCustomer(Customer customer) {
+
+    }
+
+    @Override
+    public void deleteCustomer(int customerId) {
+
+    }
 }
