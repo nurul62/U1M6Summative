c7bbfff0f515023256d86cde9ff5d0d3ada1caec
Perri Love <perri.love1188@gmail.com>
2019-07-08 11:37:56 -0400

    finished CustomerDao Interface, InvoiceDao Interface, CustomerDaoImpl, InvoiceDaoImpl

--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/InvoiceDao.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/dao/InvoiceDao.java
@@ -1,4 +1,22 @@
 package com.company.U1M6Summative.dao;
 
+import com.company.U1M6Summative.model.Invoice;
+
+import java.util.List;
+
 public interface InvoiceDao {
+
+    Invoice addInvoice (Invoice invoice);
+
+    Invoice getInvoice (int invoiceId);
+
+    List<Invoice> getAllInvoices();
+
+    List<Invoice> getInvoicesByCustomer(int customerId);
+
+    void updateInvoice(Invoice invoice);
+
+    void deleteInvoice(int invoiceId);
+
+
 }
